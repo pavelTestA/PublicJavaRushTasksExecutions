476b87cdf17867f635ec4a80c84c1eb5579c30f3
pavelTestA <124916509+pavelTestA@users.noreply.github.com>
2023-02-09 22:08:23 +0300

    Delete Archiver directory

--- Archiver/command/Command.java
+++
@@ -1,5 +0,0 @@
-package com.javarush.task.task31.Archiver.command;
-
-public interface Command {
-    void execute() throws Exception;
-}
--- Archiver/exception/PathIsNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.javarush.task.task31.Archiver.exception;
-
-public class PathIsNotFoundException extends Exception {
-}
