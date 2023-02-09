7f50682b7f621d77c042697b1bdd15e025f97c55
pavelTestA <124916509+pavelTestA@users.noreply.github.com>
2023-02-09 22:53:35 +0300

    Delete SokobanGame/SokobanGame directory

--- SokobanGame/SokobanGame/model/Direction.java
+++
@@ -1,8 +0,0 @@
-package com.javarush.task.task24.SokobanGame.SokobanGame.model;
-
-public enum Direction {
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
-}
--- SokobanGame/SokobanGame/model/Movable.java
+++
@@ -1,5 +0,0 @@
-package com.javarush.task.task24.SokobanGame.SokobanGame.model;
-
-public interface Movable {
-    void move(int x, int y);
-}
