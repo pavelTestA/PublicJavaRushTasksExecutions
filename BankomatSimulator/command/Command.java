c945b0a66cd232e86b291aabb31f41c2d707db32
pavelTestA <124916509+pavelTestA@users.noreply.github.com>
2023-02-09 22:08:35 +0300

    Delete BankomatSimulator directory

--- BankomatSimulator/command/Command.java
+++
@@ -1,7 +0,0 @@
-package com.javarush.task.task26.BankomatSimulator.command;
-
-import com.javarush.task.task26.BankomatSimulator.exception.InterruptOperationException;
-
-interface Command {
-    void execute() throws InterruptOperationException;
-}
--- BankomatSimulator/exception/NotEnoughMoneyException.java
+++
@@ -1,4 +0,0 @@
-package com.javarush.task.task26.BankomatSimulator.exception;
-
-public class NotEnoughMoneyException extends Exception{
-}
